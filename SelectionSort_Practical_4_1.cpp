584ef95d35b3602991fcf6c17e78acc891e4b200
Suyash Hirekerur <shirekerur22@outlook.com>
2026-02-06 15:20:59 +0530

    .

---
+++ SelectionSort_Practical_4_1.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter total number of elements: ";
+    cin >> n;
+    
+    int arr[n];
+ 
+    cout << "\nEnter array elements:\n";
+    for(int i = 0; i < n; i++)
+    {
+        cout << "Element " << (i+1) << ": ";
+        cin >> arr[i];
+    }
+  
+    int original[n];
+    for(int i = 0; i < n; i++)
+    {
+        original[i] = arr[i];
+    }
+
+    for(int i = 0; i < n-1; i++)
+    {
+        int minIndex = i;          
+
+        for(int j = i+1; j < n; j++)
+        {
+            if(arr[j] < arr[minIndex])
+            {
+                minIndex = j;  
+            }
+        }
+       
+        int temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+    
+    cout << "\n\n--- ORIGINAL ARRAY ---\n";
+    for(int i = 0; i < n; i++)
+    {
+        cout << original[i] << " ";
+    }
+    
+    cout << "\n\n--- SORTED ARRAY ---\n";
+    for(int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    
+    cout << "\n\n";
+    return 0;
+}
